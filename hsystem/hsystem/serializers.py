496d3426884352955dacab90fa63c3e6cbed1f9b
KenMwaura1 <kemwaura@gmail.com>
2024-05-19 19:46:17 +0300

    feat: Add serializers for Department, Patient, Doctor, and Treatment

---
+++ hsystem/hsystem/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from .models import Department, Patient, Doctor, Treatment
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Department
+        fields = ['id', 'name', 'description']
+
+class PatientSerializer(serializers.ModelSerializer):
+    department = DepartmentSerializer(read_only=True)
+
+    class Meta:
+        model = Patient
+
+class DoctorSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()
+    department = DepartmentSerializer(read_only=True)
+
+    class Meta:
+        model = Doctor
+        fields = ['id', 'user', 'department', 'specialty']
+
+class TreatmentSerializer(serializers.ModelSerializer):
+    doctor = DoctorSerializer(read_only=True)
+    patient = PatientSerializer(read_only=True)
+
+    class Meta:
+        model = Treatment
+        fields = ['id', 'doctor', 'patient', 'start_date', 'end_date']
