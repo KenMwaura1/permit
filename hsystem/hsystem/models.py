2b8a9b1e995563d8c4f457e435e1db45ad1ab61e
KenMwaura1 <kemwaura@gmail.com>
2024-05-19 19:54:50 +0300

    feat: Update medical_record_number field in Patient model
    The commit updates the `medical_record_number` field in the `Patient` model of the hospital system project. The field was previously defined as a mode, but it has been corrected to be a `CharField` with a maximum length of 20 characters.

--- hsystem/hsystem/models.py
+++ hsystem/hsystem/models.py
@@ -12,7 +12,7 @@ class Patient(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
-    medical_record_number = mode
+    medical_record_number = models.CharField(max_length=20)
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
