8363a05d5529002486470aa021d5787f63b33541
KenMwaura1 <kemwaura@gmail.com>
2024-05-19 23:31:52 +0300

    feat: Update PatientDetailView to use PatientSerializer

--- hsystem/hsystem/serializers.py
+++ hsystem/hsystem/serializers.py
@@ -1,16 +1,21 @@
 from rest_framework import serializers
 from .models import Department, Patient, Doctor, Treatment
 
+
 class DepartmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Department
         fields = ['id', 'name', 'description']
 
+
 class PatientSerializer(serializers.ModelSerializer):
     department = DepartmentSerializer(read_only=True)
+    
 
     class Meta:
         model = Patient
+        fields = '__all__'  
+
 
 class DoctorSerializer(serializers.ModelSerializer):
     user = serializers.StringRelatedField()
@@ -19,6 +24,7 @@ class DoctorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Doctor
         fields = ['id', 'user', 'department', 'specialty']
+
 
 class TreatmentSerializer(serializers.ModelSerializer):
     doctor = DoctorSerializer(read_only=True)
