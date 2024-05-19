8a7ddf4f1e0a7713423fc4ce564746bb75c43982
KenMwaura1 <kemwaura@gmail.com>
2024-05-19 19:46:45 +0300

    feat: Add patient detail view

--- hsystem/hsystem/urls.py
+++ hsystem/hsystem/urls.py
@@ -4,7 +4,9 @@ URL configuration for hsystem project.
 """
 from django.contrib import admin
 from django.urls import path
+from .views import PatientDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+	 path('patients/<int:patient_id>/', PatientDetailView.as_view(), name='patient-detail'),
 ]
